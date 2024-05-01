8130701cf5ab0046a4f310380fa147ff687cf14b
Dosdos, Josiephous Pierre G <142465754+JosiephousPierre@users.noreply.github.com>
2024-05-02 02:50:30 +0800

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://thisisclinic:thisispassword@159.65.128.24:3306/uic_clinic"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://thisisclinic:thisispassword@159.65.128.24/uic_clinic"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, echo=True
